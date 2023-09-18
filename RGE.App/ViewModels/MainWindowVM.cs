ca8822b014ed50d0991868b72d51ab7a62f516d4
Jarred Capellman <jcapellman@hotmail.com>
2023-09-18 07:13:01 -0500

    Added: Initial work on UI

---
+++ RGE.App/ViewModels/MainWindowVM.cs
@@ -0,0 +1,9 @@
+﻿using RGE.App.ViewModels.Base;
+
+namespace RGE.App.ViewModels
+{
+    public class MainWindowVM : BaseViewModel
+    {
+        public string GameHeaderStr => "Jack Sazerack (Version 0.0.1)";
+    }
+}
