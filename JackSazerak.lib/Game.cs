d314bcbf5c46582648c8b1816c00c6ef4bf9f52b
Jarred Capellman <jcapellman@hotmail.com>
2023-09-18 20:52:05 -0500

    Added: Initial commit to the plugin system

---
+++ JackSazerak.lib/Game.cs
@@ -0,0 +1,9 @@
+﻿using RGE.Lib.Abstractions;
+
+namespace JackSazerak.lib
+{
+    public class Game : BaseGame
+    {
+        public override string GameHeaderStr => "Jack Sazerack (Version 0.0.1)";
+    }
+}
--- RGE.App/ViewModels/MainWindowVM.cs
+++ RGE.App/ViewModels/MainWindowVM.cs
@@ -1,9 +1,54 @@
-﻿using RGE.App.ViewModels.Base;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using RGE.App.ViewModels.Base;
+using RGE.Lib.Abstractions;
 
 namespace RGE.App.ViewModels
 {
     public class MainWindowVM : BaseViewModel
     {
-        public string GameHeaderStr => "Jack Sazerack (Version 0.0.1)";
+        public BaseGame Game
+        {
+            get => _game;
+
+            set { _game = value; OnPropertyChanged(); }
+        }
+
+        private BaseGame _game;
+
+        private BaseGame? LoadGame()
+        {
+            var potentialGames = Directory.GetFiles(AppContext.BaseDirectory, "*.dll");
+
+            foreach (var file in potentialGames)
+            {
+                try
+                {
+                    var assembly = Assembly.LoadFile(file);
+
+                    var game = assembly.GetTypes().FirstOrDefault(a => a.BaseType == typeof(BaseGame));
+
+                    if (game != null)
+                    {
+                        return (BaseGame)Activator.CreateInstance(game)!;
+                    }
+                } catch (Exception) { }
+            }
+
+            return null;
+        }
+
+        public MainWindowVM()
+        {
+            var game = LoadGame();
+
+            if (game is not null)
+            {
+                Game = game;
+            }
+        }
     }
 }
