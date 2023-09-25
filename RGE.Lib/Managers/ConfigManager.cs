b148dd9c5e0f7dd494bc8357311315b4804ab031
Jarred Capellman <jcapellman@hotmail.com>
2023-09-24 21:09:26 -0500

    Added: ConfigManager class

---
+++ RGE.Lib/Managers/ConfigManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+using RGE.Lib.Managers.Base;
+using RGE.Lib.Objects.Config;
+
+namespace RGE.Lib.Managers
+{
+    public class ConfigManager : BaseManager
+    {
+        private static Configuration CreateDefaultConfig(string configPath)
+        {
+            var config = new Configuration();
+
+            SaveConfigAsync(configPath, config);
+
+            return config;
+        }
+
+        public static async Task<Configuration> LoadConfigAsync(string configPath)
+        {
+            if (!File.Exists(configPath))
+            {
+                return CreateDefaultConfig(configPath);
+            }
+
+            var configText = await File.ReadAllTextAsync(configPath);
+
+            var config = JsonSerializer.Deserialize<Configuration>(configText);
+
+            return config ?? CreateDefaultConfig(configPath);
+        }
+
+        public static async void SaveConfigAsync(string configPath, Configuration config)
+        {
+            await File.WriteAllTextAsync(configPath, System.Text.Json.JsonSerializer.Serialize(config));
+        }
+    }
+}
--- RGE.Lib/Objects/Config/Configuration.cs
+++ RGE.Lib/Objects/Config/Configuration.cs
@@ -1,20 +1,29 @@
-﻿using RGE.Lib.Abstractions.Base;
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+using RGE.Lib.Abstractions.Base;
 using RGE.Lib.Enums;
 
 namespace RGE.Lib.Objects.Config
 {
     public class Configuration : BaseRGEObject
     {
+        [JsonIgnore]
         public override string Name => nameof(Configuration);
 
+        [JsonIgnore]
         public override ObjectTypes ObjectType => ObjectTypes.JsonObject;
 
+        [DefaultValue("OpenGL")]
         public string vid_ref { get; set; }
 
+        [DefaultValue(800)]
         public int vid_xres { get; set; }
 
+        [DefaultValue(600)]
         public int vid_yres { get; set; }
 
+        [DefaultValue(false)]
         public bool vid_fullscreen { get; set; }
     }
 }
