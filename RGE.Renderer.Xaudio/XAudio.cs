ac4ee6bc76bcf32c2c1bd15acf1175a317a401a4
Jarred Capellman <jcapellman@hotmail.com>
2023-10-02 07:05:13 -0500

    Added: XAudio Project

---
+++ RGE.Renderer.Xaudio/XAudio.cs
@@ -0,0 +1,15 @@
+﻿using RGE.Lib.Abstractions.Renderers;
+using RGE.Lib.Objects.Config;
+
+namespace RGE.Renderer.Xaudio
+{
+    public class XAudio : BaseSoundRenderer
+    {
+        public override string Name => "XAudio";
+
+        public override bool Init(Configuration config)
+        {
+            return true;
+        }
+    }
+}
